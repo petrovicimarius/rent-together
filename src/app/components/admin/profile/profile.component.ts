a4970efef9604c1cc1634d4dc977060fbf2f1f99
Marius Petrovici <marius.petrovici@assist.ro>
2019-06-22 14:44:39 +0300

    [profile] -Profile

--- src/app/components/admin/profile/profile.component.ts
+++ src/app/components/admin/profile/profile.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiConnectionService } from '@services/api-connection.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  userData = {
+    name: '',
+    email: '',
+    position: '',
+    department: '',
+    phone: '',
+    image: '',
+  };
+
+  message: string;
+  imgURL: any;
+
+  constructor(
+    private _service: ApiConnectionService<any>,
+  ) { }
 
   ngOnInit() {
+    // this.getUser();
+  }
+
+  getUser(): void {
+    const cleaner = this._service.get(`${environment.base}${environment.users}${'3'}`).subscribe(res => {
+      this.userData = res.user;
+      cleaner.unsubscribe();
+    },
+      err => {
+        console.error("ERROR getting user profile: ", err);
+      });
+  }
+
+  preview(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.userData.image = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 
 }
